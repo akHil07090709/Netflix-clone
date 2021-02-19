1a05ec0f0dd0d1f75fbd368a8f40b96a10ee3cfd
Akhilkumar Boragala <boragala.akhilkumar@gmail.com>
2021-02-19 06:39:50 +0530

    1v. release

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCipckczB7UdLYXIJLr3EJGNJYHhtW-WcA",
+  authDomain: "netflix-clone-1bef8.firebaseapp.com",
+  projectId: "netflix-clone-1bef8",
+  storageBucket: "netflix-clone-1bef8.appspot.com",
+  messagingSenderId: "413861271421",
+  appId: "1:413861271421:web:0b7b627b97be51e0979e14",
+};
